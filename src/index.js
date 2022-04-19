32d929f1acbb883d8bcaa7b9a534312d0bfd6acd
Chrissy1209 <chrissy93881209@gmail.com>
2022-04-19 11:47:45 +0800

    0419

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import './index.css';
 // import Home from './pages/Home';//前面的名稱必須使用大寫英文為開頭
 // import Header from './componets/header';
 // import Footer from './componets/footer';
+
+
+
+
+
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,7 +19,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
