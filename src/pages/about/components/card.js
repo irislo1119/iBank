c72d4bd2775060ac955b443ab27e36d0c23cf5b1
Chrissy1209 <chrissy93881209@gmail.com>
2022-04-23 18:54:09 +0800

    0423

---
+++ src/pages/about/components/card.js
@@ -0,0 +1,51 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col,Card } from 'react-bootstrap';
+
+var wall = {
+    border: "1px solid #9E9E9E",
+    // boxShadow: "3px 3px 7px #9E9E9E",
+    margin:"0 10px"
+}
+const cardBodyHeight = {
+    // height: "280px"
+}
+const cardPadding = {
+    padding: '10px 10px 20px 10px',
+}
+const textMargin = {
+    marginTop: "15px",
+    marginBottom: "4px",
+}
+const label = {
+    // color: 'steelblue',
+    textDecoration:'none'
+}
+
+const MyCard = () => {
+    <Col style={cardPadding}>
+        <Card border="light" style={wall}>
+            <Card.Body style={cardBodyHeight}>
+                <Card.Title>許靜sss玟</Card.Title>
+                <Card.Text style={textMargin}>
+                    系級：元智大學
+                </Card.Text>
+                <Card.Text>
+                    負責內容：負責內容負責內容負責內容，負責內容負責內容  
+                </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+                <small className="text-muted">
+                    <span>GitHub: </span>
+                    <a href='https://github.com/Chrissy1209' style={label}>https://github.com/Chrissy1209</a>
+                </small>
+                <br></br>
+                <small>
+                    <span style={{color:"#6c757d"}}>Email: </span>
+                    <a href='mailto: chrissyhsu.i@gmail.com' style={label}>chrissyhsu.i@gmail.com</a>
+                </small>
+            </Card.Footer>
+        </Card>
+    </Col>
+}
+
+export default MyCard;
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,62 +1,59 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container,Row,Col,CardGroup,Card } from 'react-bootstrap';
+import MyCard from './components/card';
 
 const backGroundColor = {
-    // backgroundColor: "aliceblue",
+    backgroundColor: "aliceblue",
+}
+const title = {
+    // color: "steelblue",
+    paddingTop: 30,
+    textAlign: "center",
+}
+const text = {
+    paddingBottom: 10,
+    paddingTop: 10
 }
 var wall = {
-    boxShadow: "3px 3px 7px #9E9E9E",
+    border: "1px solid #9E9E9E",
+    // boxShadow: "3px 3px 7px #9E9E9E",
     margin:"0 10px"
 }
 const cardBodyHeight = {
     // height: "280px"
 }
 const cardPadding = {
-    padding: '20px 10px',
-}
-
-const label = {
-    color: 'steelblue',
-    textDecoration:'none'
-}
-const titlePadding = {
-    padding: '20px 0 0 20px',
-}
-const title = {
-    color: "steelblue",
-    paddingBottom: 15,
-}
-const aboutUs = {
-    margin:'30px 0 0 20px',
-    color: "steelblue",
-    padding:0,
+    padding: '10px 10px 20px 10px',
 }
 const textMargin = {
     marginTop: "15px",
-    marginBottom: "5px",
+    marginBottom: "4px",
 }
-
+const label = {
+    // color: 'steelblue',
+    textDecoration:'none'
+}
 const About = () => {
     return(
         <Container fluid>
-            <Row className="d-flex justify-content-center align-item-center border" style={backGroundColor}>
-                <Col xs={12} style={titlePadding}>
-                    <h5 style={title}>設計動機</h5>
+            <Row style={backGroundColor} className="d-flex justify-content-center align-item-center border">
+                <Col xs={12}>
+                    <h5 style={title} className="align-item-center">設計動機</h5>
                 </Col>
                 <Col xs={10}>
-                    <p>成年後，有不少人開始會管理自己的銀行帳號，學習投資、資金管理等。銀行的網站及app也成為我們經常會使用的工具之一，因此我們想要從生活出發，做一個簡易的銀行系統。</p>
-                </Col>
-                <Col xs={12} style={titlePadding}>
-                    <h5 style={title}>目的</h5>
-                </Col>
-                <Col xs={10}>
-                    <p>    提供用戶轉帳、查帳等功能</p>
+                    <p style={text}>成年後，有不少人開始會學習投資、管理自己的銀行帳號等。因此我們想要從生活出發，做一個簡易的銀行系統。</p>
                 </Col>
             </Row>
             <Row className="border">
-                <h5 style={aboutUs} className="w-50">關於我們</h5>
+                <Col xs={12}>
+                    <h5 style={title} className="align-item-center">關於我們</h5>
+                </Col>
                 <CardGroup>
-                    <Col xs style={cardPadding}>
+                    <MyCard />
+                    <MyCard />
+                    <MyCard />
+                    
+                    {/* <Col xs style={cardPadding}>
                         <Card border="light" style={wall}>
                             <Card.Body style={cardBodyHeight}>
                                 <Card.Title>許靜玟</Card.Title>
@@ -79,7 +76,7 @@ const About = () => {
                                 </small>
                             </Card.Footer>
                         </Card>
-                    </Col>
+                    </Col> */}
                     <Col xs style={cardPadding}>
                         <Card border="light" bg="light" style={wall}>
                             <Card.Body style={cardBodyHeight}>
@@ -104,28 +101,7 @@ const About = () => {
                             </Card.Footer>
                         </Card>
                     </Col>
-                    <Col xs style={cardPadding}>
-                        <Card border="light" style={wall}>
-                            <Card.Body style={cardBodyHeight}>
-                                <Card.Title>Name</Card.Title>
-                                <Card.Text style={textMargin}>
-                                    This is a wider card with supporting text below as a natural lead-in to
-                                    additional content. This content is a little bit longer.
-                                </Card.Text>
-                            </Card.Body>
-                            <Card.Footer>
-                                <small className="text-muted">
-                                    <span>GitHub: </span>
-                                    <a href='url' style={label}>url</a>
-                                </small>
-                                <br></br>
-                                <small>
-                                    <span style={{color:"#6c757d"}}>Email: </span>
-                                    <a href='mailto: Email' style={label}>Email</a>
-                                </small>
-                            </Card.Footer>
-                        </Card>
-                    </Col>
+                   
                     <Col xs style={cardPadding}>
                         <Card border="light" bg="light" style={wall}>
                             <Card.Body style={cardBodyHeight}>
@@ -148,7 +124,7 @@ const About = () => {
                             </Card.Footer>
                         </Card>
                     </Col>
-                    </CardGroup>
+                </CardGroup>
             </Row>
         </Container>
     ); 
